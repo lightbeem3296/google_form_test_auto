027336621caa6537df48df5e1dd1e8264318ba60
Billy Lights <alpha5611331@gmail.com>
2024-04-24 21:28:12 +0900

    multi instance

--- crawler.py
+++ crawler.py
@@ -11,6 +11,18 @@ from pyrogram.client import Client
 from libchrome import Chrome
 from liblogger import log_err, log_inf
 
+ACCOUNT_LIST = {
+    "alpha0@gmail.com": {
+        "name": "alpha0",
+    },
+    "alpha1@gmail.com": {
+        "name": "alpha1",
+    },
+    "alpha2@gmail.com": {
+        "name": "alpha2",
+    },
+}
+
 API_ID = 20992264
 API_HASH = "8506019a33a5f425e40fd9d119c511a8"
 TELEGRAM_APP = Client("my_account", API_ID, API_HASH)
@@ -18,7 +30,7 @@ TELEGRAM_APP = Client("my_account", API_ID, API_HASH)
 
 CUR_DIR = str(Path(__file__).parent.absolute())
 TEMP_DIR = os.path.join(CUR_DIR, "temp")
-CHROME: Chrome
+CHROME_LIST = {}
 
 
 @TELEGRAM_APP.on_message(filters.text)
@@ -31,19 +43,14 @@ async def hello(client, message):
             for url in urls:
                 if not "forms.gle" in url:
                     continue
-
-                submit_google_form_test(
-                    url=url,
-                    shuffle_name=message.from_user.username,
-                    telegram_name=message.from_user.username,
-                )
+                submit_all(url=url)
         else:
             log_err(f"{message.from_user.username}: {message.text}")
 
 
-def init_chrome() -> Chrome:
+def init_chrome(profile_name: str) -> Chrome:
     log_inf("open chrome browser")
-    chrome = Chrome(user_data_dir=os.path.join(TEMP_DIR, "profile"))
+    chrome = Chrome(user_data_dir=os.path.join(TEMP_DIR, f"profile_{profile_name}"))
     chrome.start()
 
     # login google account
@@ -56,28 +63,38 @@ def init_chrome() -> Chrome:
     return chrome
 
 
-def submit_google_form_test(url: str, shuffle_name: str, telegram_name: str):
+def submit_all(url: str):
+    for key in CHROME_LIST:
+        submit_google_form_test(
+            chrome=CHROME_LIST[key],
+            url=url,
+            shuffle_name=ACCOUNT_LIST[key]["name"],
+            telegram_name=ACCOUNT_LIST[key]["name"],
+        )
+
+
+def submit_google_form_test(chrome: Chrome, url: str, shuffle_name: str, telegram_name: str):
     try:
         # go to google form test url and wait until title "Google Form Test" is selecable
         log_inf("open google form test url")
-        CHROME.goto(url2go=url, wait_elem_selector="div.F9yp7e.ikZYwf.LgNcQe")
+        chrome.goto(url2go=url, wait_elem_selector="div.F9yp7e.ikZYwf.LgNcQe")
 
         # click check box
-        if CHROME.run_script("document.querySelector('#i5').getAttribute('aria-checked')") == "false":
-            CHROME.click(selector="label.docssharedWizToggleLabeledContainer.OLkl6c")
+        if chrome.run_script("document.querySelector('#i5').getAttribute('aria-checked')") == "false":
+            chrome.click(selector="label.docssharedWizToggleLabeledContainer.OLkl6c")
 
         # input shuffle name
         b64_shuffle_name = base64.b64encode(shuffle_name.encode()).decode()
-        CHROME.run_script(f"document.querySelectorAll('input.whsOnd.zHQkBf')[0].value=atob('{b64_shuffle_name}')")
-        CHROME.run_script("document.querySelectorAll('div.ndJi5d.snByac')[0].innerText=''")
+        chrome.run_script(f"document.querySelectorAll('input.whsOnd.zHQkBf')[0].value=atob('{b64_shuffle_name}')")
+        chrome.run_script("document.querySelectorAll('div.ndJi5d.snByac')[0].innerText=''")
 
         # input telegram name
         b64_telegram_name = base64.b64encode(telegram_name.encode()).decode()
-        CHROME.run_script(f"document.querySelectorAll('input.whsOnd.zHQkBf')[1].value=atob('{b64_telegram_name}')")
-        CHROME.run_script("document.querySelectorAll('div.ndJi5d.snByac')[1].innerText=''")
+        chrome.run_script(f"document.querySelectorAll('input.whsOnd.zHQkBf')[1].value=atob('{b64_telegram_name}')")
+        chrome.run_script("document.querySelectorAll('div.ndJi5d.snByac')[1].innerText=''")
 
         # input captcha
-        math_eq = CHROME.run_script(
+        math_eq = chrome.run_script(
             """
 elems = document.querySelectorAll('span.M7eMe');
 elem = elems[elems.length - 1];
@@ -90,26 +107,30 @@ elem.innerText"""
             eq_res = str(eval(math_eq))
             log_inf(f"result: {eq_res}")
 
-            b64_captcha = base64.b64encode(eq_res.encode()).decode()
-            CHROME.run_script(f"document.querySelectorAll('input.whsOnd.zHQkBf')[2].value=atob('{b64_captcha}')")
-            CHROME.run_script("document.querySelectorAll('div.ndJi5d.snByac')[2].innerText=''")
+            b64_eq_res = base64.b64encode(eq_res.encode()).decode()
+            chrome.run_script(f"document.querySelectorAll('input.whsOnd.zHQkBf')[2].value=atob('{b64_eq_res}')")
+            chrome.run_script("document.querySelectorAll('div.ndJi5d.snByac')[2].innerText=''")
 
         time.sleep(0.5)
         # click submit button
-        CHROME.run_script("document.querySelectorAll('span.NPEfkd.RveJvd.snByac')[0].click()")
+        chrome.run_script("document.querySelectorAll('span.NPEfkd.RveJvd.snByac')[0].click()")
     except:
         traceback.print_exc()
 
 
 def work():
-    global CHROME
+    global CHROME_LIST
     try:
-        # open chrome browser
-        CHROME = init_chrome()
+        # open chrome browsers
+        for key in ACCOUNT_LIST:
+            chrome = init_chrome(key)
+            CHROME_LIST[key] = chrome
 
         TELEGRAM_APP.run()
 
-        CHROME.quit()
+        # close chrome browsers
+        for key in CHROME_LIST:
+            CHROME_LIST[key].quit()
     except:
         traceback.print_exc()
 
@@ -119,15 +140,19 @@ def main():
 
 
 def test():
-    global CHROME
-    CHROME = init_chrome()
-    submit_google_form_test(
-        url="https://forms.gle/4NccFM5EcL12mdnM7",
-        shuffle_name="ekudahl",
-        telegram_name="ekudahl",
-    )
+    # open chrome browsers
+    for key in ACCOUNT_LIST:
+        chrome = init_chrome(key)
+        CHROME_LIST[key] = chrome
+
+    submit_all(url="https://forms.gle/4NccFM5EcL12mdnM7")
+    submit_all(url="https://forms.gle/4NccFM5EcL12mdnM7")
+
     input("Press ENTER to close browser.")
-    CHROME.quit()
+
+    # close chrome browsers
+    for key in CHROME_LIST:
+        CHROME_LIST[key].quit()
 
 
 if __name__ == "__main__":
